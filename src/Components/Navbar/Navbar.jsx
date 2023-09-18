bb18a2c44766599f8055d4da96274ce761c27318
Maximiliano Failla <faillamaximiliano@gmail.com>
2023-09-18 10:53:02 -0300

    Condition Moon/Sun Icon, and Profile Avatar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,18 +10,26 @@ import {
   NavbarMenuToggle,
   NavbarMenu,
   NavbarMenuItem,
+  Avatar,
 } from "@nextui-org/react";
 import Logo from "./Logo";
 import { NavLink } from "react-router-dom";
-const Navbarr = ({ theme, toggleTheme }) => {
+import MoonIcon from "../Icons/MoonIcon/MoonIcon";
+import SunIcon from "../Icons/SunIcon/SunIcon";
+const Navbarr = ({ theme, toggleTheme, user }) => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
 
   const menuItems = [
     { name: "Home", route: "/" },
     { name: "Products", route: "/products" },
     { name: "Cart", route: "/cart" },
     { name: "Profile", route: "/profile" },
-    { name: "Log out", route: "/logout" },
+    { name: "Log out", route: "/login" },
   ];
   return (
     <Navbar onMenuOpenChange={setIsMenuOpen}>
@@ -40,25 +48,48 @@ const Navbarr = ({ theme, toggleTheme }) => {
 
       <NavbarContent justify="end">
         <NavbarItem className="flex gap-4">
-          <Button onClick={toggleTheme}>Toggle Theme</Button>
-          <Button as={Link} href="#">
-            Enter Account
+          <Button onClick={toggleTheme} isIconOnly variant="bordered">
+            {theme === "light" ? <MoonIcon /> : <SunIcon />}
           </Button>
+          {localStorage.getItem("user") ? (
+            <Avatar
+              isBordered
+              color="secondary"
+              src={JSON.parse(localStorage.getItem("user")).photoURL}
+            />
+          ) : (
+            <Button as={Link} href="/login">
+              Enter Account
+            </Button>
+          )}
         </NavbarItem>
       </NavbarContent>
 
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              color={index === menuItems.length - 1 ? "danger" : "foreground"}
-              className="w-full"
-              href={item.route}
-              size="lg"
-              key={index}
-            >
-              {item.name}
-            </Link>
+            {item.name === "Log out" ? (
+              <Link
+                color="danger"
+                className="w-full"
+                href={item.route}
+                size="lg"
+                key={index}
+                onClick={logout}
+              >
+                {item.name}
+              </Link>
+            ) : (
+              <Link
+                color={index === menuItems.length - 1 ? "danger" : "foreground"}
+                className="w-full"
+                href={item.route}
+                size="lg"
+                key={index}
+              >
+                {item.name}
+              </Link>
+            )}
           </NavbarMenuItem>
         ))}
       </NavbarMenu>
