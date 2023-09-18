cd2a7d890da28a23b36040cba807136a46a45416
Maximiliano Failla <faillamaximiliano@gmail.com>
2023-09-18 10:54:05 -0300

    Route Profile

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./Components/Home/Home";
 import ProductDetail from "./Components/Products/ProductDetail";
 import Products from "./Components/Products/Products";
 import Navbar from "./Components/Navbar/Navbar";
+import SignIn from "./Components/Auth/SignIn/SignIn";
 
 export default function App() {
   const [theme, setTheme] = React.useState(() => {
@@ -12,6 +13,7 @@ export default function App() {
     } */
     return "light";
   });
+  const [user, setUser] = React.useState(null);
 
   const toggleTheme = () => {
     setTheme((theme) => (theme === "light" ? "dark" : "light"));
@@ -26,11 +28,18 @@ export default function App() {
   }, [theme]);
   return (
     <>
-      <Navbar theme={theme} toggleTheme={toggleTheme} />
+      <Navbar theme={theme} toggleTheme={toggleTheme} user={user} />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/products" component={Products} />
         <Route exact path="/products/:id" component={ProductDetail} />
+        <Route
+          exact
+          path="/login"
+          render={(props) => (
+            <SignIn {...props} setUser={setUser} user={user} />
+          )}
+        />
       </Switch>
     </>
   );
