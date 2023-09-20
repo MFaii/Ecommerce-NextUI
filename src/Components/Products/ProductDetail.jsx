2e7080559ddedf1504c05d21388d223b3c4a9550
Maximiliano Failla <faillamaximiliano@gmail.com>
2023-09-20 19:07:53 -0300

    modified styles on detail

--- src/Components/Products/ProductDetail.jsx
+++ src/Components/Products/ProductDetail.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { NavLink } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { Button } from "@nextui-org/react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+
 const ProductDetail = () => {
   const { id } = useParams();
-  const [product, setProduct] = React.useState([]);
+  const [product, setProduct] = React.useState(null);
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -20,33 +20,62 @@ const ProductDetail = () => {
 
     fetchProduct();
   }, [id]);
+
   const ShowProductDetail = () => {
+    if (!product) {
+      return null;
+    }
+
     return (
-      <div className="flex flex-wrap justify-center items-center h-screen">
-        <div className="fixedCard">
-          <img src={product.image} alt={product.title} />
+      <div
+        style={{
+          display: "grid",
+          placeItems: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <div className="w-1/2 p-4">
+          <div
+            className="w-64 h-80 mx-auto"
+            style={{
+              border: "1px solid #ddd",
+              borderRadius: "8px",
+              overflow: "hidden",
+              maxWidth: "300px",
+              maxHeight: "400px",
+            }}
+          >
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-full h-full object-cover"
+            />
+          </div>
         </div>
-        <div className="flex flex-col justify-center gap-4 p-4 text-center">
-          <h2>{product.title}</h2>
-          <p>Price: ${product.price}</p>
-          <p>Description: {product.description}</p>
-          <Button color="primary" variant="ghost">
-            Add to Cart <AiOutlineShoppingCart />
-          </Button>
-          <NavLink to="/products">
-            <Button color="secondary" variant="ghost">
-              Back to Products
-            </Button>
-          </NavLink>
+        <div className="w-1/2 p-4">
+          <div className="max-w-md mx-auto">
+            <h2 className="text-2xl font-bold mb-4">{product.title}</h2>
+            <p className="text-lg">Price: ${product.price}</p>
+            <div className="text-lg max-h-40 overflow-y-auto">
+              {product.description}
+            </div>
+            <div className="flex justify-center gap-4">
+              <Button color="primary" variant="ghost" className="mt-4">
+                Add to Cart <AiOutlineShoppingCart className="ml-2" />
+              </Button>
+              <Link to="/products" className="mt-4 block">
+                <Button color="secondary" variant="ghost">
+                  Back to Products
+                </Button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     );
   };
-  return (
-    <>
-      <ShowProductDetail />
-    </>
-  );
+
+  return <ShowProductDetail />;
 };
 
 export default ProductDetail;
